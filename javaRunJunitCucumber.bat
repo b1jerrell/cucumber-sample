d3d3ab7aad4e666e7909f7b806748cc7e2ae89b3
b1jerrell <87187174+b1jerrell@users.noreply.github.com>
2022-03-21 13:14:47 -0400

    Update javaRunJunitCucumber.bat

--- javaRunJunitCucumber.bat
+++ javaRunJunitCucumber.bat
@@ -1,1 +1,1 @@
-java -Dfile.encoding=utf-8 -cp D:/Demo/cucumber-sample/resources/libs/*;D:/Demo/cucumber-sample/target/*; org.junit.runner.JUnitCore "com.glue.RunTest"
+java -Dfile.encoding=utf-8 -cp C:/Users/Administrator/Demo/cucumber-sample/resources/libs/*;C:/Users/Administrator/Demo/cucumber-sample/target/*; org.junit.runner.JUnitCore "com.glue.RunTest"
