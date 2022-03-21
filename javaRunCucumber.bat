2fa4c26875b3f441949a386cb2eec7696817fb6d
b1jerrell <87187174+b1jerrell@users.noreply.github.com>
2022-03-21 13:13:43 -0400

    Update javaRunCucumber.bat

--- javaRunCucumber.bat
+++ javaRunCucumber.bat
@@ -1,1 +1,1 @@
-java -Dfile.encoding=utf-8 -cp D:/Demo/cucumber-sample/resources/libs/*;D:/Demo/cucumber-sample/target/*; cucumber.api.cli.Main -g "com/glue" -p "pretty" "D:/Demo/cucumber-sample/resources/feature"
+java -Dfile.encoding=utf-8 -cp C:/Users/Administrator/Demo/cucumber-sample/resources/libs/*;C:/Users/Administrator/Demo/cucumber-sample/target/*; cucumber.api.cli.Main -g "com/glue" -p "pretty" "C:/Users/Administrator/Demo/cucumber-sample/resources/feature"
